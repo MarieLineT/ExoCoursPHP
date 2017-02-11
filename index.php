ea712884ab3f487e6ee1ff5ab45c18ec7d211d51
MarieLineT <mtemps@ensc.fr>
2017-02-11 21:34:34 +0100

    Ajouté index.php et mon_script_php.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mon formulaire</title>
+    </head>
+    <body>
+        <form action="ma_page.html" method="POST">
+            <label for="field_1">Exemple de champs de formulaire </label>
+            <input type="text" name="field_1" value="Tapez du texte" />
+
+            <input type="submit" value="Envoyer" />
+        </form> 
+
+    </body>
+</html>
---
+++ mon_script_php.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>Bonjour à tous !</h1>";
+
+?>
